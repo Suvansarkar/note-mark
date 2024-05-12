8738dfb4eff34a00a2f21b209b9bdbf69e3d7638
Suvansarkar <suvansarkar1@gmail.com>
2024-05-12 20:30:11 +0530

    Added left panel (Note Preview and Note Preview List) - UI Only

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { ActionButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from "@/components"
+import { ActionButtonsRow, Content, DraggableTopBar, NotePreviewList, RootLayout, Sidebar } from "@/components"
 
 const App = () => {
   return (
@@ -7,6 +7,7 @@ const App = () => {
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1 " />
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -1,6 +1,14 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'IST'
+})
+
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
+
 export const cn = (...args: ClassValue[]) => {
   return twMerge(clsx(...args))
 }
