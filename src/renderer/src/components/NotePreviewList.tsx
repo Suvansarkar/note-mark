12b80fcda90750a7b97fde8a48e9d91edfd1b667
Suvansarkar <suvansarkar1@gmail.com>
2024-05-14 14:24:52 +0530

    Add hooks and make the NotePreviewList interactive with the user

--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,11 +1,16 @@
 import { notesMock } from "@renderer/store/mocks"
 import { ComponentProps } from "react"
-import { NotePreview } from "./NotePreview"
+import { NotePreview } from "@/components/NotePreview"
 import { twMerge } from "tailwind-merge"
+import { useNotesList } from "@/hooks/useNotesList"
 
-export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) => {
+export type NotePreviewListProps = ComponentProps<'ul'> & {
+  onSelect?: () => void
+}
 
-  if (notesMock.length === 0) {
+export const NotePreviewList = ({ onSelect, className, ...props }: NotePreviewListProps) => {
+  const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
+  if (notes.length === 0) {
     return (
       <ul className={twMerge('text-center pt-4', className)} {...props}>
         <span>No Notes Yet!</span>
@@ -15,8 +20,11 @@ export const NotePreviewList = ({ className, ...props }: ComponentProps<'ul'>) =
 
   return (
     <ul className={className} {...props}>
-      {notesMock.map((note) => (
-        <NotePreview key={note.title + note.lastEditTime} {...note} />
+      {notesMock.map((note, index) => (
+        <NotePreview key={note.title + note.lastEditTime}
+          isActive={selectedNoteIndex == index}
+          onClick={handleNoteSelect(index)}
+          {...note} />
       ))}
     </ul>
   )
