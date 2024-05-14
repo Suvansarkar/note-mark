97bf53fc3572f8670b655bd472640f43014efb51
Suvansarkar <suvansarkar1@gmail.com>
2024-05-14 12:49:04 +0530

    Markdown editor UI + functionality and note title compopnents

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { ActionButtonsRow, Content, DraggableTopBar, NotePreviewList, RootLayout, Sidebar } from "@/components"
+import { ActionButtonsRow, Content, DraggableTopBar, FloatingNoteTitle, MarkdownEditor, NotePreviewList, RootLayout, Sidebar } from "@/components"
 
 const App = () => {
   return (
@@ -9,7 +9,10 @@ const App = () => {
           <ActionButtonsRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1 " />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
